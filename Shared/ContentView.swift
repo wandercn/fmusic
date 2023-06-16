27015183faf0759a54bf551f8497b357d33fa166
wander <wander@ffactory.org>
2023-06-16 17:35:24 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by lsmiao on 2023/6/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  Shared
+//
+//  Created by lsmiao on 2023/6/16.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
