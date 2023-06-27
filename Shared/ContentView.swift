52e2dc54c44f95275c123c78002615aadd94d5d8
wander <wander@ffactory.org>
2023-06-27 18:14:32 +0800

    Update ContentView.swift
    增加自定义修饰

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,32 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @Binding var paths: [String]
-    @State var currnetSong: Song = Song(name: "", artist: "", album: "", duration: TimeInterval(0), filePath: "", isSelected: false)
+    //    @Binding var paths: [String]
+    @State var currnetSong: Song = Song(name: "", artist: "", album: "", duration: TimeInterval(0), filePath: "", isSelected: false, isPlaying: false)
     @Binding  var libraryList:[Song]
-//    @State var librayList = [
-//        Song(name: "阳光宅男1", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男2", artist: "周杰伦2", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男3", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男4", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男5", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男6", artist: "周杰伦", album: "我很忙2", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男7", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男8", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男9", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男10", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男11", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男12", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男13", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男14", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男15", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男16", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男17", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男18", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男19", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男20", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//        Song(name: "阳光宅男21", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
-//    ]
+    //    @State var librayList = [
+    //        Song(name: "阳光宅男1", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男2", artist: "周杰伦2", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男3", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男4", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男5", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男6", artist: "周杰伦", album: "我很忙2", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男7", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男8", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男9", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男10", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男11", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男12", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男13", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男14", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男15", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男16", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男17", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男18", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男19", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男20", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //        Song(name: "阳光宅男21", artist: "周杰伦", album: "我很忙", duration: 233,isSelected: false),
+    //    ]
     
     var body: some View {
         VStack{
@@ -41,11 +41,14 @@ struct ContentView: View {
             ListContentView(currnetSong: self.$currnetSong, libraryList:self.$libraryList)
             PlayerView(libraryList: self.$libraryList, currnetSong: self.$currnetSong)
             
-//            ContentView1()
+            //            ContentView1()
         }
-                .frame(minWidth: 850, minHeight: 600 )
+        .frame(minWidth: 850, minHeight: 600 )
+        
     }
+    
 }
+
 
 //struct ContentView_Previews: PreviewProvider {
 //    static var previews: some View {
@@ -55,6 +58,77 @@ struct ContentView: View {
 //}
 
 
+struct BlueBackgroundOnHover: ViewModifier {
+    @State private var isHovered = false
+    
+    func body(content: Content) -> some View {
+        content
+            .background(isHovered ? Color.blue : Color.clear)
+            .foregroundColor(isHovered ? Color.white : .secondary)
+            .clipShape(
+                Circle(
+                    
+                    
+                )
+            )
+            .onHover { isHovered in
+                withAnimation {
+                    self.isHovered = isHovered
+                }
+            }
+    }
+}
 
+struct PinkBackgroundOnHover: ViewModifier {
+    @State private var isHovered = false
+    
+    func body(content: Content) -> some View {
+        content
+            .background(isHovered ? Color.pink : Color.clear)
+            .foregroundColor(isHovered ? Color.white : .secondary)
+            .clipShape(
+                Circle()
+            )
+            .onHover { isHovered in
+                withAnimation {
+                    self.isHovered = isHovered
+                }
+            }
+    }
+}
+
+extension View {
+    func pinkBackgroundOnHover() -> some View {
+        self.modifier(PinkBackgroundOnHover())
+    }
+}
+
+
+
+struct ItemBackgroundOnHover: ViewModifier {
+    @State private var isHovered = false
+    
+    func body(content: Content) -> some View {
+        content
+            .background(isHovered ? Color.yellow: Color.clear)
+            
+            .clipShape(
+                Capsule()
+            )
+            .scaleEffect(isHovered ? 0.97 : 1.0)
+            .onHover { isHovered in
+                withAnimation {
+                    self.isHovered = isHovered
+                }
+            }
+    }
+}
+
+
+extension View {
+    func itemBackgroundOnHover() -> some View {
+        self.modifier(ItemBackgroundOnHover())
+    }
+}
 
 
