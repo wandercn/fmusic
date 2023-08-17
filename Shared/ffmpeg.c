c196634262d67bb58c905cbbb6aa15c331cadc31
wander <wander@ffactory.org>
2023-08-17 18:03:24 +0800

    Update ffmpeg.c
    能编辑flac mp4 m4a等文件,其他可能修改失败

--- Shared/ffmpeg.c
+++ Shared/ffmpeg.c
@@ -39,9 +39,9 @@ AVFormatContext * open_audio_file_fmt_ctx(const char *filename) {
     while ((tag = av_dict_get(fmt_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)))
         //        printf("%s=%s\n", tag->key, tag->value);
         av_log(NULL, AV_LOG_DEBUG, "[%d]<%s>%s tag: %s = %s\n",LOG_HEAD,tag->key, tag->value);
-    if (DEBUG) {
+#if DEBUG
     av_dump_format(fmt_ctx,0,0,0);
-    }
+#endif
     
     return fmt_ctx;
 }
@@ -78,121 +78,182 @@ AVPacket *  get_album_cover_image(const char *filename){
 
 int modify_meta(const char* in_filename,const char* out_filename ,AVDictionary * new_metadata)
 {
-    const AVOutputFormat *ofmt = NULL;
+    const AVOutputFormat *oformat = NULL;
     AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
     AVPacket *pkt = NULL;
+    const AVCodec *audio_dec = NULL, *video_dec = NULL;
     int ret, i;
     int stream_index = 0;
     int *stream_mapping = NULL;
     int stream_mapping_size = 0;
     pkt = av_packet_alloc();
     if (!pkt) {
-     av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Could not allocate AVPacket\n",LOG_HEAD);
+        av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Could not allocate AVPacket\n",LOG_HEAD);
         return -1;
     }
-
+    
     if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
-     av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Could not open input file: %s\n",LOG_HEAD,in_filename);
+        av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Could not open input file: %s\n",LOG_HEAD,in_filename);
         goto end;
-    }
-
-    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
-     av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed to retrieve input stream information\n",LOG_HEAD);
-        goto end;
-    }
-    if (DEBUG) {
-    av_dump_format(ifmt_ctx, 0, in_filename, 0);
     }
     
-    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
+    av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &video_dec, 0);
+    av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, &audio_dec, 0);
+    
+    
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+        av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed to retrieve input stream information\n",LOG_HEAD);
+        goto end;
+    }
+#if DEBUG
+    
+    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+#endif
+    
+    
+    
+    oformat =  av_guess_format(&ifmt_ctx->iformat->extensions[0], in_filename, ifmt_ctx->iformat->mime_type);
+    avformat_alloc_output_context2(&ofmt_ctx,oformat,ifmt_ctx->iformat->extensions, out_filename);
     if (!ofmt_ctx) {
         av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Could not create output context\n",LOG_HEAD);
         ret = AVERROR_UNKNOWN;
         goto end;
     }
-
+    
     stream_mapping_size = ifmt_ctx->nb_streams;
     stream_mapping = av_calloc(stream_mapping_size, sizeof(*stream_mapping));
     if (!stream_mapping) {
         ret = AVERROR(ENOMEM);
         goto end;
     }
-
-    ofmt = ofmt_ctx->oformat;
-
+    
+    printf("ofmart: %s\n",oformat->name);
+    printf("ofmart: %s\n",&oformat->extensions[0]);
+    printf("ofmart: %s\n",oformat->mime_type);
+    printf("ofmart: %d\n",oformat->subtitle_codec);
+    printf("ofmart: %d\n",oformat->audio_codec);
     for (i = 0; i < ifmt_ctx->nb_streams; i++) {
         AVStream *out_stream;
         AVStream *in_stream = ifmt_ctx->streams[i];
         AVCodecParameters *in_codecpar = in_stream->codecpar;
-
+        
         if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
             in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
             in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
             stream_mapping[i] = -1;
             continue;
         }
-
+        
         stream_mapping[i] = stream_index++;
-
+        
         out_stream = avformat_new_stream(ofmt_ctx, NULL);
         if (!out_stream) {
             av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed allocating output stream\n",LOG_HEAD);
             ret = AVERROR_UNKNOWN;
             goto end;
         }
-
+        
         ret = avcodec_parameters_copy(out_stream->codecpar, in_codecpar);
         if (ret < 0) {
             av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed to copy codec parameters\n",LOG_HEAD);
             goto end;
         }
-        out_stream->codecpar->codec_tag = 0;
+        //        out_stream->codecpar->codec_tag = 0;
+        // copy 每个stream中的metaData
+        ret = av_dict_copy(&out_stream->metadata, in_stream->metadata, AV_DICT_DONT_OVERWRITE);
+        
+        if (ret < 0) {
+            av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed to copy metadata\n",LOG_HEAD);
+            goto end;
+        }
+        
+        if (in_stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO){
+            ret = av_packet_copy_props(&out_stream->attached_pic, &in_stream->attached_pic);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed to copy attached_pic\n",LOG_HEAD);
+                goto end;
+            }
+            
+        }
+        out_stream->av_class = in_stream->av_class;
+        out_stream->disposition = in_stream->disposition;
+        out_stream->duration = in_stream->duration;
+        out_stream->start_time = in_stream->start_time;
+        out_stream->sample_aspect_ratio = in_stream->sample_aspect_ratio;
+        out_stream->r_frame_rate = in_stream->r_frame_rate;
+        out_stream->avg_frame_rate = in_stream ->avg_frame_rate;
+        out_stream->codecpar = in_stream->codecpar;
+        out_stream->time_base = in_stream ->time_base;
+        out_stream->pts_wrap_bits = in_stream->pts_wrap_bits;
+        
+        
     }
+    // copy 原始的元信息
+    ret = av_dict_copy(&ofmt_ctx->metadata, ifmt_ctx->metadata, AV_DICT_DONT_OVERWRITE);
+    
+    if (ret < 0) {
+        av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Failed to copy metadata\n",LOG_HEAD);
+        goto end;
+    }
+    
+    ofmt_ctx->duration = ifmt_ctx->duration;
+    ofmt_ctx->start_time = ifmt_ctx->start_time;
+    ofmt_ctx->bit_rate = ifmt_ctx->bit_rate;
+    ofmt_ctx->flags = ifmt_ctx->flags;
+    ofmt_ctx->nb_streams = ifmt_ctx->nb_streams;
+    ofmt_ctx->chapters = ifmt_ctx->chapters;
+    ofmt_ctx->nb_chapters = ifmt_ctx->nb_chapters;
+    av_dict_set(&ofmt_ctx->metadata, "duration", NULL, 0);
+    av_dict_set(&ofmt_ctx->metadata, "creation_time", NULL, 0);
+    av_dict_set(&ofmt_ctx->metadata, "company_name", NULL, 0);
+    av_dict_set(&ofmt_ctx->metadata, "product_name", NULL, 0);
+    av_dict_set(&ofmt_ctx->metadata, "product_version", NULL, 0);
     const AVDictionaryEntry *next = NULL;
     // 设置新的元信息
     while ((next = av_dict_iterate(new_metadata, next))){
         av_dict_set(&ofmt_ctx->metadata, next->key, next->value, 0);
     }
-     
-    if (DEBUG) {
+    
+#if DEBUG
     av_dump_format(ofmt_ctx, 0, out_filename, 1);
-    }
-
-    if (!(ofmt->flags & AVFMT_NOFILE)) {
+#endif
+    
+    if (!(oformat->flags & AVFMT_NOFILE)) {
         ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
         if (ret < 0) {
             av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Could not open output file '%s'\n",LOG_HEAD,out_filename);
             goto end;
         }
     }
-
-    ret = avformat_write_header(ofmt_ctx, NULL);
+    avformat_queue_attached_pictures(ofmt_ctx);
+    
+    ret = avformat_write_header(ofmt_ctx, &new_metadata);
     if (ret < 0) {
         av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Error occurred when opening output file\n",LOG_HEAD);
         goto end;
     }
-
+    
     while (1) {
         AVStream *in_stream, *out_stream;
-
+        
         ret = av_read_frame(ifmt_ctx, pkt);
         if (ret < 0)
             break;
-
+        
         in_stream  = ifmt_ctx->streams[pkt->stream_index];
         if (pkt->stream_index >= stream_mapping_size ||
             stream_mapping[pkt->stream_index] < 0) {
             av_packet_unref(pkt);
             continue;
         }
-
+        
         pkt->stream_index = stream_mapping[pkt->stream_index];
         out_stream = ofmt_ctx->streams[pkt->stream_index];
-
+        
         /* copy packet */
         av_packet_rescale_ts(pkt, in_stream->time_base, out_stream->time_base);
         pkt->pos = -1;
-
+        
         ret = av_interleaved_write_frame(ofmt_ctx, pkt);
         /* pkt is now blank (av_interleaved_write_frame() takes ownership of
          * its contents and resets pkt), so that no unreferencing is necessary.
@@ -202,25 +263,26 @@ int modify_meta(const char* in_filename,const char* out_filename ,AVDictionary *
             break;
         }
     }
-
+    
     av_write_trailer(ofmt_ctx);
+    av_log(NULL, AV_LOG_INFO, "[%d]<%s>%s %s - 修改元信息成功\n",LOG_HEAD,in_filename);
 end:
     av_packet_free(&pkt);
-
+    
     avformat_close_input(&ifmt_ctx);
-
+    
     /* close output */
-    if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+    if (ofmt_ctx && !(oformat->flags & AVFMT_NOFILE)){
         avio_closep(&ofmt_ctx->pb);
-    avformat_free_context(ofmt_ctx);
-
+    }
+    
     av_freep(&stream_mapping);
-
+    
     if (ret < 0 && ret != AVERROR_EOF) {
         av_log(NULL, AV_LOG_ERROR, "[%d]<%s>%s Error occurred: %s\n",LOG_HEAD,av_err2str(ret));
         return -1;
     }
-
+    
     return 0;
 }
 
